f121323ba3867274ec4b304bda1cd4cb07857ca4
Darya <dashaandreeva012016@gmail.com>
2024-07-20 01:22:50 +0300

    updt

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -16,4 +16,10 @@ const usersSchema = new Schema(
   { timestamps: true, versionKey: false }
 );
 
+usersSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export const UsersCollection = model('users', usersSchema);
