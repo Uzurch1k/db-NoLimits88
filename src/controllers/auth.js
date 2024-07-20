ec9619c20c089a3f539c9eba0960a870c2f17812
Olena <kukushonka@gmail.com>
2024-07-20 16:52:03 +0200

    add refresh token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 import { registerUser } from '../services/auth.js';
-import { loginUser } from '../services/auth.js';
+import { loginUser, refreshUsersSession } from '../services/auth.js';
 import { ONE_DAY } from '../constants/index.js';
 
 export const registerUserController = async (req, res) => {
@@ -27,6 +27,30 @@ export const loginUserController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
+};
+
+export const refreshUserSessionController = async (req, res) => {
+  const session = await refreshUsersSession({
+    sessionId: req.cookies.sessionId,
+    refreshToken: req.cookies.refreshToken,
+  });
+
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date(Date.now() + ONE_DAY),
+  });
+
+  res.json({
+    status: 200,
+    message: 'Successfully refreshed a session',
     data: {
       accessToken: session.accessToken,
     },
