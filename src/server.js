f5ee53b2cc0486f2ca910ca2b2f97d30b56c650c
Darya <dashaandreeva012016@gmail.com>
2024-07-20 15:35:00 +0300

    sign-in

--- src/server.js
+++ src/server.js
@@ -1,38 +1,41 @@
-import express from 'express'; 
+import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', '3000'));
 
 export const setupServer = () => {
-    const app = express();
+  const app = express();
 
-    app.use(express.json({
-        type: ['application/json', 'application/vnd.api+json'],
-        }),
-    );
-    app.use(cors());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+    })
+  );
+  app.use(cors());
 
-    app.use(
-        pino({
-            transport: {
-                target: 'pino-pretty',
-            },
-        }),
-    );
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    })
+  );
 
-    app.use(router);
+  app.use(cookieParser());
 
+  app.use(router);
 
-    app.use('*', notFoundHandler);
+  app.use('*', notFoundHandler);
 
-    app.use(errorHandler);
-    
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,10 @@
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
+import { randomBytes } from 'crypto';
+
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
+import { SessionsCollection } from '../db/models/session.js';
 
 export const registerUser = async payload => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -11,5 +15,30 @@ export const registerUser = async payload => {
   return await UsersCollection.create({
     ...payload,
     password: encryptedPassword,
+  });
+};
+
+export const loginUser = async payload => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+
+  await SessionsCollection.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
 };
